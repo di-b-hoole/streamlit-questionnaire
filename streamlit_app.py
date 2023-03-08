0d41d4f170f8cca5b5df82c3e5974216bd50f4a0
di-b-hoole <123636281+di-b-hoole@users.noreply.github.com>
2023-03-08 13:39:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,31 @@
+import streamlit as st
 
+def main():
+    st.set_page_config(page_title="My Streamlit App")
+
+    # Define your menu options here
+    menu = ["Home", "Page 1", "Page 2"]
+
+    choice = st.sidebar.selectbox("Select a page", menu)
+
+    if choice == "Home":
+        home()
+    elif choice == "Page 1":
+        page1()
+    elif choice == "Page 2":
+        page2()
+
+def home():
+    st.title("Welcome to my app!")
+    st.write("Please select a page from the menu.")
+
+def page1():
+    st.title("Page 1")
+    st.write("This is the first page of my app.")
+
+def page2():
+    st.title("Page 2")
+    st.write("This is the second page of my app.")
+
+if __name__ == "__main__":
+    main() #test
