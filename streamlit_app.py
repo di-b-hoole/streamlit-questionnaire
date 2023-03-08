5c1f836f8a2026a879630b19f20f820f2f847b38
di-b-hoole <b.hoole@decisioninc.com>
2023-03-08 17:10:42 +0200

    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,13 @@ def init_connection():
         **st.secrets["snowflake"], client_session_keep_alive=True
     )
 
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+conn = init_connection()
 
 def main():
     st.set_page_config(page_title="My Streamlit App")
@@ -25,6 +32,7 @@ def main():
     elif choice == "Page 2":
         page2()
 
+    
     rows = run_query("SELECT TOP 5 * FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.REGION;")
 
     # Display the results in a Streamlit table
@@ -33,6 +41,22 @@ def main():
 def home():
     st.title("Welcome to my app!")
     st.write("Please select a page from the menu.")
+
+    # Define the questions
+    questions = [
+        'What is your name?',
+        'How old are you?',
+        'Where are you from?',
+        'What is your favorite color?'
+    ]
+
+    # Initialize the answers
+    answers = {}
+
+    # Loop through the questions and get user input
+    for question in questions:
+        answer = st.text_input(question)
+        answers[question] = answer
 
 def page1():
     st.title("Page 1")
@@ -43,6 +67,6 @@ def page2():
     st.write("This is the second page of my app.")
 
 if __name__ == "__main__":
-    main() #test
+    main()
 
 
