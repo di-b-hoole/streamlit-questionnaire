004e71aeff5a4a96a4cecee9be643022ec32f529
Brendanlearnings <brendanlearnings@gmail.com>
2023-03-10 14:15:25 +0200

    please work man

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,8 +142,8 @@ def Answers():
         
         rows = run_query("SELECT ID,NAME,AGE,NO_DOGS,NO_CATS,NO_BIRDS,NO_FISH,NO_REPTILES,IS_PREDICTION FROM ANSWERS;")
         
-        answers_df = pd.DataFrame(rows, columns = ['ID','NAME','AGE','NO_DOGS','NO_CATS','NO_BIRDS','NO_FISH','NO_REPTILES','IS_PREDICTION'])
-        answers_df = answers_df.set_index('ID')
+        answers_df = pd.DataFrame(rows, columns = ['ID','NAME','NO_DOGS','NO_CATS','NO_BIRDS','NO_FISH','NO_REPTILES','IS_PREDICTION'])
+        #answers_df = answers_df.set_index('ID')
         # Display the results in a Streamlit table
         st.table(answers_df)
 
